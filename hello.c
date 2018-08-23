401e87e293924ebb7ebf3575fe7a044b4b88a2c7
tsallinger1993 <42591399+tsallinger1993@users.noreply.github.com>
2018-08-23 16:08:41 -0500

    Hack-1

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Anthony Sallinger, Max Einfalt, Fengrui Liu
+ * Date: 2018/08/21
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Anthony Sallinger, Max Einfalt, Fengrui Liu\n");
+
+  return 0;
+}
